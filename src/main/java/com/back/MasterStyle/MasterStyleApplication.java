a3e2dce3c1ca476561d7c6b366244f5c794bebb5
JulianDAGUZ <juliandaguzz@gmail.com>
2023-12-15 19:48:44 -0500

    Add

---
+++ src/main/java/com/back/MasterStyle/MasterStyleApplication.java
@@ -0,0 +1,13 @@
+package com.back.MasterStyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterStyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/MasterStyle/MasterStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.MasterStyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
